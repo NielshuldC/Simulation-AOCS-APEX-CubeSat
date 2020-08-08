0cca2de44b8dc3a4b49de96b7ac831dcfecbe923
NielshuldC <69392488+NielshuldC@users.noreply.github.com>
2020-08-08 19:09:35 +0200

    Update master_spaceCAN.py

--- CAN_bus/SpaceCAN_application/master_spaceCAN.py
+++ CAN_bus/SpaceCAN_application/master_spaceCAN.py
@@ -1,1 +1,47 @@
+import pyb
+import time
+import logging; logging.get_logger('spacecan').set_level(logging.DEBUG)
 import spacecan
+
+led = pyb.LED(3)
+
+button = pyb.Pin(pyb.Pin.board.SW, pyb.Pin.IN)
+
+def debounce(pin):
+    cur_value = pin.value()
+    active = 0
+    while active < 20:
+        if pin.value() != cur_value:
+            active += 1
+        else:
+            active = 0
+        pyb.delay(1)  
+
+
+MY_NODE_ID = 0  # master node has id 0
+SLAVE_NODE_IDS = [123]  # list of all attached slave nodes
+
+bus_a, bus_b = spacecan.Bus(1), spacecan.Bus(2)
+network = spacecan.Network(MY_NODE_ID, bus_a, bus_b)
+network.set_filters([  # receive telemetry frames from all slaves
+    (spacecan.ID_TM + node_id, spacecan.FULL_MASK)
+    for node_id in SLAVE_NODE_IDS])
+heartbeat = spacecan.HeartbeatProducer(network)
+sync = spacecan.SyncProducer(network)
+telecommand = spacecan.TelecommandProvider(network)
+telemetry = spacecan.TelemetryConsumer(network)
+
+network.start()
+heartbeat.start(500)
+sync.start(5000)
+
+
+def main():
+    while True:
+        debounce(button)
+            print("Send telecommand, mode:", mode)
+            telecommand.send(SLAVE_NODE_IDS[0], mode)
+            mode = "OFF" if mode == "ON" else "ON"
+
+main()  # run the main loop
+
